db919ccbc42524bbd6a6be7f2ea1982fda050c43
Tobin C. Harding <me@tobin.cc>
2023-08-08 18:47:05 +1000

    Add bitcoin-units crate
    Add a new crate `bitcoin-units`, move the `amount` module over to it and
    re-export all types from `bitcoin::amount` so this as not a breaking
    change.

--- bitcoin/src/lib.rs
+++ bitcoin/src/lib.rs
@@ -96,7 +96,6 @@ mod serde_utils;
 #[macro_use]
 pub mod p2p;
 pub mod address;
-pub mod amount;
 pub mod base58;
 pub mod bip152;
 pub mod bip158;
@@ -145,6 +144,36 @@ pub use crate::{
     sighash::{EcdsaSighashType, TapSighashType},
     taproot::{TapBranchTag, TapLeafHash, TapLeafTag, TapNodeHash, TapTweakHash, TapTweakTag},
 };
+
+pub mod amount {
+    //! Bitcoin amounts.
+    //!
+    //! This module mainly introduces the [Amount] and [SignedAmount] types.
+    //! We refer to the documentation on the types for more information.
+
+    #[cfg(feature = "serde")]
+    pub use units::amount::serde;
+    pub use units::amount::{
+        Amount, CheckedSum, Denomination, Display, ParseAmountError, SignedAmount,
+    };
+
+    use crate::consensus::{encode, Decodable, Encodable};
+    use crate::io;
+
+    impl Decodable for Amount {
+        #[inline]
+        fn consensus_decode<R: io::Read + ?Sized>(r: &mut R) -> Result<Self, encode::Error> {
+            Ok(Amount::from_sat(Decodable::consensus_decode(r)?))
+        }
+    }
+
+    impl Encodable for Amount {
+        #[inline]
+        fn consensus_encode<W: io::Write + ?Sized>(&self, w: &mut W) -> Result<usize, io::Error> {
+            self.to_sat().consensus_encode(w)
+        }
+    }
+}
 
 #[cfg(not(feature = "std"))]
 mod io_extras {
